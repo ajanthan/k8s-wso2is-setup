02a14512feb087459303945d2f81b0f4dc88095c
Ajanthan Balachandran <balaajanthan@gmail.com>
2018-04-20 13:34:40 -0700

    refactoring directories

--- image/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-eval $(minikube docker-env)
-docker build -t ajanthan/oauth2-sample --build-arg JAR_FILE=oauth2-login-sample-0.0.1-SNAPSHOT.jar .
--- undeploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-kubectl delete configmap app-config
-kubectl delete -f k8s/oauth2-sample-rc.yaml 
-kubectl delete -f k8s/oauth2-sample-svc.yaml 
-kubectl delete -f k8s/oauth2-sample-ingress.yaml 
